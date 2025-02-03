49dcc98006b13094c96bfac369cac93bad97d61a
Manh Nguyen <manhbapper190704@gmail.com>
2025-02-03 21:44:52 +0700

    video 30

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require('express') //commonJS
 const configViewEngine = require('./config/viewEngine') 
 const webRouter = require('./routes/web')
 
+const mysql = require('mysql2')
+
 const app = express() //app express
 const port = process.env.PORT || 8888//port => hardcode
 const hostname = process.env.HOST_NAME
@@ -12,6 +14,29 @@ configViewEngine(app)
 
 // khai báo route
 app.use('/', webRouter)
+
+//test connect mysql
+// Create the connection to database
+async function connectToDatabase() {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit'
+  });
+
+  // simple query
+  connection.query(
+    'SELECT * from Users u',
+    function(err, results, fields) {
+      console.log(">>> results= ", results); // results contains rows returned by server
+      console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
+    }
+  );
+}
+
+connectToDatabase();
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
