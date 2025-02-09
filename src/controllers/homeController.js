61592d4e0ad1121439748a8bdd3aa192054dc6e5
Manh Nguyen <manhbapper190704@gmail.com>
2025-02-09 12:08:31 +0700

    video 33

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,20 @@
+const connection = require('../config/database')
+
 
 const getHomePage = (req, res) => {
     //process data
     //call model
-    res.send('Hello World with Manhbapper!')
+    let users = [];
+
+    connection.query(
+  'SELECT * from Users u',
+function(err, results, fields) {
+        users = results;
+        console.log(">>> results= ", results); // results contains rows returned by server
+        res.send(JSON.stringify(users))
+    }
+);
+    
 }
 const getABC = (req, res) => {
     res.send('Check abc!')
--- src/server.js
+++ src/server.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const express = require('express') //commonJS
 const configViewEngine = require('./config/viewEngine') 
 const webRouter = require('./routes/web')
-
-const mysql = require('mysql2')
+const connection = require('./config/database')
 
 const app = express() //app express
 const port = process.env.PORT || 8888//port => hardcode
@@ -16,27 +15,13 @@ configViewEngine(app)
 app.use('/', webRouter)
 
 //test connect mysql
-// Create the connection to database
-async function connectToDatabase() {
-  const connection = await mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123456',
-    database: 'hoidanit'
-  });
-
-  // simple query
-  connection.query(
-    'SELECT * from Users u',
-    function(err, results, fields) {
-      console.log(">>> results= ", results); // results contains rows returned by server
-      console.log(">>> fields= ", fields); // fields contains extra meta data about results, if available
-    }
-  );
-}
-
-connectToDatabase();
+// simple query
+connection.query(
+  'SELECT * from Users u',
+  function(err, results, fields) {
+    console.log(">>> results= ", results); // results contains rows returned by server
+  }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
